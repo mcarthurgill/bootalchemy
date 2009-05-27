60fdc502a785327663c665cdfdef5cd16a85fbf0
percious <devnull@localhost>
2009-05-27 12:22:49 -0600

    fixed yaml dep.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name='bootalchemy',
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
-          'PyYaml'
+          'PyYAML'
       ],
       entry_points="""
       # -*- Entry points: -*-
