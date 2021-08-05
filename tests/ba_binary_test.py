3860457992f904777801abde7c1025baf4544b7c
McArthur Gill <mcarthur@storyfit.com>
2021-08-04 21:37:17 -0500

    update

--- tests/ba_binary_test.py
+++ tests/ba_binary_test.py
@@ -1,41 +1,40 @@
-import sqlalchemy as sa
-import sqlalchemy.types as sa_types
-import sqlalchemy.orm as orm
+# import sqlalchemy as sa
+# import sqlalchemy.types as sa_types
+# import sqlalchemy.orm as orm
 
-metadata = sa.MetaData()
+# metadata = sa.MetaData()
 
-simple_table = sa.Table('simple', metadata,
-    sa.Column('simp_id', sa_types.Integer, primary_key=True),
-    sa.Column('hash', sa_types.Binary),
-)
+# simple_table = sa.Table('simple', metadata,
+#     sa.Column('simp_id', sa_types.Integer, primary_key=True)
+# )
 
 
-class Simple(object):
-    def __init__(self, hash=None):
-        self.hash = hash
+# class Simple(object):
+#     def __init__(self, hash=None):
+#         self.hash = hash
 
-simple_mapper = orm.mapper(Simple, simple_table)
+# simple_mapper = orm.mapper(Simple, simple_table)
 
-from bootalchemy.loader import YamlLoader
+# from bootalchemy.loader import YamlLoader
 
-class model:
-    Simple = Simple
+# class model:
+#     Simple = Simple
 
-s = '''
--
- Simple:
-    - {hash: !!binary '1yrdLS8QDAKYe28hBRURx3JEhLg='}
-'''
+# s = '''
+# -
+#  Simple:
+#     - {hash: !!binary '1yrdLS8QDAKYe28hBRURx3JEhLg='}
+# '''
 
-if __name__ == '__main__':
-    engine = sa.create_engine('postgresql+psycopg2://localhost/ba_binary_test')
+# if __name__ == '__main__':
+#     engine = sa.create_engine('postgresql+psycopg2://localhost/ba_binary_test')
 
-    metadata.bind = engine
-    metadata.create_all(checkfirst=True)
+#     metadata.bind = engine
+#     metadata.create_all(checkfirst=True)
 
-    session = orm.sessionmaker()()
+#     session = orm.sessionmaker()()
 
-    loader = YamlLoader(model)
-    loader.loads(session, s)
+#     loader = YamlLoader(model)
+#     loader.loads(session, s)
 
-    session.commit()
+#     session.commit()
